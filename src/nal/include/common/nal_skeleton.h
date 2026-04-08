3f27e36bd7454ca2c6def1fc3c44b7019c419421
USM <166993675+UltimateSpider-Man@users.noreply.github.com>
2026-04-08 22:29:18 +0200

    Add inverse kinematics functions and structures

--- src/nal/include/common/nal_skeleton.h
+++ src/nal/include/common/nal_skeleton.h
@@ -1,5 +1,10 @@
 #pragma once
 
+struct vector3d;
+struct vector4d;
+struct matrix4x4;
+struct quaternion;
+
 #include <cstdint>
 
 #include <fixedstring.h>
@@ -24,3 +29,97 @@ struct nalBaseSkeleton {
 
 //0x0078DC80
 extern void *nalConstructSkeleton(void *a1);
+
+namespace inverse_kinematics {
+    extern void __cdecl nalIKMap2DTo3D(
+        float chain_scale,
+        float sin0,
+        float cos0,
+        float sin1,
+        float cos1,
+        vector3d* origin,
+        vector3d* bone_axis_dir,
+        vector4d* bend_dir,
+        float chain_sin0,
+        float chain_cos0,
+        matrix4x4* joint0,
+        matrix4x4* joint1);
+
+    extern void __cdecl nalIKSolve2D(
+        matrix4x4* hinge,
+        vector3d* root,
+        vector3d* target,
+        float b0a_len,
+        float b1a_len,
+        float b0b_len,
+        float b1b_len,
+        vector3d* proj_point,
+        vector3d* bone_axis_dir,
+        float* sin0,
+        float* cos0,
+        float* sin1,
+        float* cos1);
+
+    struct ik_bone_chain_t
+    {
+        float b0a_len;
+        float b1a_len;
+        float b0b_len;
+        float b1b_len;
+        float chain_scale;
+    };
+    using get_bend_dir_t = vector3d * (__cdecl*)(
+        vector3d* outTmp,
+        matrix4x4* line_xform,
+        matrix4x4* effector,
+        float       bendX,
+        float       bendY,
+        float       bendZ);
+
+    extern void __cdecl solve_two_bone(
+        matrix4x4* j0,
+        matrix4x4* j1,
+        matrix4x4* line_xform,
+        vector3d* root,
+        matrix4x4* effector,
+        ik_bone_chain_t* chain,
+        get_bend_dir_t get_bend_dir);
+
+    extern void __cdecl DecomposeIKSpin(
+        matrix4x4* joint0,
+        matrix4x4* joint1,
+        matrix4x4* hinge,
+        vector3d* root,
+        matrix4x4* effector,
+        ik_bone_chain_t* chain,
+        get_bend_dir_t get_bend_dir,
+        float                   twistAngle);
+
+    extern vector4d* __cdecl compute_bend_plane_normal(
+        vector4d* out,
+        float*     /*unused*/,
+        matrix4x4* m,
+        float      axis_x,
+        float      axis_y,
+        float      axis_z);
+
+    extern vector3d* __cdecl compute_arm_elbow_bend_direction(
+        vector3d* out,
+        matrix4x4* m,
+        matrix4x4* /*ent*/,
+        float      dirX,
+        float      dirY,
+        float      dirZ);
+
+    extern vector3d* __cdecl compute_arm_elbow_bend_direction_mirrored(
+        vector3d* out,
+        matrix4x4* m,
+        matrix4x4* /*ent*/,
+        float      dirX,
+        float      dirY,
+        float      dirZ);
+
+    extern quaternion* __cdecl quat_blend(
+        quaternion* quat, quaternion* quatA, float* weightA, quaternion* quatB, vector4d* weights);
+
+}
